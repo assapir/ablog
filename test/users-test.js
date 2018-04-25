7ae9b122340827916069b3080c0d9c888ebb0cd2
Assaf Sapir <assapir@microsoft.com>
2018-04-25 09:52:32 +0300

    tests passing:)

--- test/users-test.js
+++ test/users-test.js
@@ -5,26 +5,25 @@ const sinon = require(`sinon`);
 const MongoClient = require(`mongo-mock`).MongoClient;
 const Users = require(`../lib/users`).Users;
 
+let collectionMock;
+beforeEach(async function() {
+    const client = await MongoClient.connect(`mongodb://localhost:27017/`);
+    const db = client.db(`site`);
+    collectionMock = db.collection(`users`);
+});
+
 describe(`Users class`, function() {
     describe(`addUser`, function() {
         it(`Will call db insert`, async function() {
             const username = `username`;
             const password = `password`;
 
-            const client = await MongoClient.connect(`mongodb://localhost:27017/`);
-            const db = client.db(`site`);
-            const collection = db.collection(`users`);
-
-            const collectionMock = sinon.mock(collection, `insert`);
-            collectionMock.expects(`insert`)
-                .once()
-                .withExactArgs({ username: username, password: password })
-                .resolves({ success: true });
-
+            sinon.spy(collectionMock, `insert`);
+            
             const users = new Users(collectionMock);
-            const result = await users.addUser(username, password);
-            collectionMock.verify();
-            expect(result.sucess).to.equals(true);
+            await users.addUser(username, password);
+            expect(collectionMock.insert.calledOnce);
+            collectionMock.insert.restore(); // Unwraps the spy
         });
     });
 });
